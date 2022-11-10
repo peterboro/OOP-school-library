0fa905e09bf27debc0d85c28160b7c11eececa61
Peter Njuguna <Ptahbn@gmail.com>
2022-11-10 18:43:35 +0300

    fix linters

--- student.rb
+++ student.rb
@@ -1,12 +1,16 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
-  def initialize(age, classroom: 'None', name: 'Unknown', parent_permission: true)
-    super(age, name: name, parent_permission: parent_permission)
+  def initialize(age, name: 'Unknown', parent_permission: true, classroom: nil)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
